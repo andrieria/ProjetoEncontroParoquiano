44a0b548bed5acad096c4f2947c501353ccf23d6
Andriéria Dantas <andrieria.ad@gmail.com>
2023-12-19 19:48:25 -0300

    Adicionando comentários

--- src/main/java/br/ifrn/projeto/integrador/IntegradorApplication.java
+++ src/main/java/br/ifrn/projeto/integrador/IntegradorApplication.java
@@ -3,7 +3,14 @@ package br.ifrn.projeto.integrador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*@SpringBootApplication*/
+/*Quando o servidor está ativo e adicionamos alguma dependência, ela não vai ser levada em conta. É preciso desligar 
+o servidor e depois restarta. 
+
+O Maven é responsável por gerenciar os pacotes do spring.
+
+https://ralflima.com/treinamentos/criando_apis_com_spring_boot/spring_modulo1_dependencias.php
+
+*/
 @SpringBootApplication
 public class IntegradorApplication {
 
--- src/main/java/br/ifrn/projeto/integrador/model/Equipe.java
+++ src/main/java/br/ifrn/projeto/integrador/model/Equipe.java
@@ -1,7 +1,10 @@
 package br.ifrn.projeto.integrador.model;
 
+import jakarta.persistence.Entity;
+
 /*a classe model, serve para capturar os dados, que por padrão, não são
 enviados pela URL*/ 
+@Entity
 public class Equipe {
 
     /*Quando trabalhamos com model é sempre bom deixar os atributos privados*/
